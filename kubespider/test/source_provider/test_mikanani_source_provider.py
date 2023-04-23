e3985620125f384bc1f15623b2bf4ce6f466c2fd
xiaozhikang <xiaozhikang0916@gmail.com>
2023-04-23 19:25:30 +0800

    fix lint style
    Signed-off-by: xiaozhikang <xiaozhikang0916@gmail.com>

--- kubespider/test/source_provider/test_mikanani_source_provider.py
+++ kubespider/test/source_provider/test_mikanani_source_provider.py
@@ -1,12 +1,12 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../../kubespider")
 import unittest
 import logging
 import re
+import sys
+import os
+
 from source_provider.mikanani_source_provider.provider import MikananiSourceProvider
 
-import xml.etree.ElementTree as ET
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../../kubespider")
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s-%(levelname)s: %(message)s')
 class MikkananiSouirceProviderTest(unittest.TestCase):
@@ -20,18 +20,18 @@ class MikkananiSouirceProviderTest(unittest.TestCase):
             pattern = re.compile(reg)
         else:
             pattern = None
-        with open(self.test_file, 'r') as test_file:
+        with open(self.test_file, encoding="utf-8", mode= 'r') as test_file:
             lines = test_file.readlines()
         return list(filter(lambda p: p is not None, map(lambda p: self.provider.check_anime_title(p, pattern), lines)))
 
     def test_title_filter_full(self):
-        dict = self.load(None)
-        self.assertEqual(7, len(dict))
+        title_dict = self.load(None)
+        self.assertEqual(7, len(title_dict))
 
     def test_title_filter_reg_include(self):
-        dict = self.load( ".*简.*")
-        self.assertEqual(3, len(dict))
+        title_dict = self.load( ".*简.*")
+        self.assertEqual(3, len(title_dict))
 
     def test_title_filter_reg_except(self):
-        dict = self.load( "^((?!繁).)*$")
-        self.assertEqual(4, len(dict))
+        title_dict = self.load( "^((?!繁).)*$")
+        self.assertEqual(4, len(title_dict))
