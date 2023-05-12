f2399adec44ca8b3218be87d470eb01237640f25
ijwstl <asiaw159753@outlook.com>
2023-05-13 02:32:07 +0800

    add: Support configuration reload automatically after changed, and do not need to restart the kubespider container
    Signed-off-by: ijwstl <asiaw159753@outlook.com>

--- kubespider/core/file_handler.py
+++ kubespider/core/file_handler.py
@@ -15,7 +15,6 @@ class FileHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if str(event.src_path).endswith("state.yaml") or str(event.src_path).endswith(".config"):
             return
-        change_file = event.src_path
-        logging.info(f"文件被修改了,{change_file}")
+        logging.info("文件被修改了, %s", event.src_path)
         start_file = os.path.join(os.getenv('HOME'), 'start.sh')
         os.system(f"sh {start_file}")
