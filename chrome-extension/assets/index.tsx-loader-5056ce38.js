cabd58bd006ffe4eb646de6cb6d8aa43ed6ea8eb
richardli1598 <richardli1598@users.noreply.github.com>
2023-12-19 12:07:56 +0000

    bot: build kubespider extension

--- chrome-extension/assets/index.tsx-loader-5056ce38.js
+++ chrome-extension/assets/index.tsx-loader-5056ce38.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-7e3b3ba5.js")
+      chrome.runtime.getURL("assets/chunk-13957a98.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
