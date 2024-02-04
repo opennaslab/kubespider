042ac356e62c85accbfd7bcb2ee7ff21a4948a71
miRemid <miosuki241@outlook.com>
2024-02-04 22:55:21 +0800

    fix: add wbi auth for bilibili_vlogger_subscribe_source_provider
    Signed-off-by: miRemid <miosuki241@outlook.com>

--- kubespider/source_provider/bilibili_vlogger_subscribe_source_provider/provider.py
+++ kubespider/source_provider/bilibili_vlogger_subscribe_source_provider/provider.py
@@ -9,6 +9,62 @@ from utils import helper
 from source_provider import provider
 
 
+from functools import reduce
+from hashlib import md5
+import urllib.parse
+import time
+import requests
+
+mixinKeyEncTab = [
+    46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
+    33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
+    61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
+    36, 20, 34, 44, 52
+]
+
+def getMixinKey(orig: str):
+    return reduce(lambda s, i: s + orig[i], mixinKeyEncTab, '')[:32]
+
+def encWbi(params: dict, img_key: str, sub_key: str):
+    mixin_key = getMixinKey(img_key + sub_key)
+    curr_time = round(time.time())
+    params['wts'] = curr_time                                   # 添加 wts 字段
+    params = dict(sorted(params.items()))                       # 按照 key 重排参数
+    params = {
+        k : ''.join(filter(lambda chr: chr not in "!'()*", str(v)))
+        for k, v 
+        in params.items()
+    }
+    query = urllib.parse.urlencode(params)                      # 序列化参数
+    wbi_sign = md5((query + mixin_key).encode()).hexdigest()    # 计算 w_rid
+    params['w_rid'] = wbi_sign
+    return params
+
+def getWbiKeys() -> tuple[str, str]:
+    resp = requests.get('https://api.bilibili.com/x/web-interface/nav')
+    resp.raise_for_status()
+    json_content = resp.json()
+    img_url: str = json_content['data']['wbi_img']['img_url']
+    sub_url: str = json_content['data']['wbi_img']['sub_url']
+    img_key = img_url.rsplit('/', 1)[1].split('.')[0]
+    sub_key = sub_url.rsplit('/', 1)[1].split('.')[0]
+    return img_key, sub_key
+
+# img_key, sub_key = getWbiKeys()
+#
+# signed_params = encWbi(
+#     params={
+#         'foo': '114',
+#         'bar': '514',
+#         'baz': 1919810
+#     },
+#     img_key=img_key,
+#     sub_key=sub_key
+# )
+# query = urllib.parse.urlencode(signed_params)
+# print(signed_params)
+# print(query)
+
 class BilibiliVloggerSubscribeSourceProvider(provider.SourceProvider):
     def __init__(self, name: str, config_reader: AbsConfigReader) -> None:
         super().__init__(config_reader)
@@ -17,6 +73,7 @@ class BilibiliVloggerSubscribeSourceProvider(provider.SourceProvider):
         self.webhook_enable = True
         self.provider_type = 'bilibili_vlogger_subscribe_source_provider'
         self.provider_name = name
+        self.sessdata = ""
 
     def get_provider_name(self) -> str:
         return self.provider_name
@@ -60,26 +117,38 @@ class BilibiliVloggerSubscribeSourceProvider(provider.SourceProvider):
         if isinstance(vloggers, str):
             vloggers = [vloggers]
 
-        controller = helper.get_request_controller()
+        controller = helper.get_request_controller(cookie="SESSDATA={}".format(self.sessdata))
         ret = []
+
+        resp = controller.get('https://api.bilibili.com/x/web-interface/nav')
+        resp.raise_for_status()
+        json_content = resp.json()
+        img_url: str = json_content['data']['wbi_img']['img_url']
+        sub_url: str = json_content['data']['wbi_img']['sub_url']
+        img_key = img_url.rsplit('/', 1)[1].split('.')[0]
+        sub_key = sub_url.rsplit('/', 1)[1].split('.')[0]
 
         for vlogger in vloggers:
             try:
-                data_link = "https://api.bilibili.com/x/space/wbi/arc/search?mid=" + str(vlogger)
+                signed_params = encWbi(
+                    params={"mid": int(vlogger)},
+                    img_key=img_key,
+                    sub_key=sub_key,
+                )
+                query = urllib.parse.urlencode(signed_params)
+                data_link = "https://api.bilibili.com/x/space/wbi/arc/search?" + query
                 resp = controller.get(data_link, timeout=30).json()
-
                 for video in resp['data']['list']['vlist']:
                     path = video['title']
                     link = "https://www.bilibili.com/video/" + video['bvid']
                     file_type = types.FILE_TYPE_VIDEO_MIXED
-                    logging.info("BilibiliVloggerSubscribeSourceProvider get links %s", link)
-                    ret.append({'path': path, 'link': link, 'file_type': file_type})
                     ret.append(Resource(
                         url=link,
                         path=path,
                         file_type=file_type,
                         link_type=self.get_link_type(),
                     ))
+                    logging.info("BilibiliVloggerSubscribeSourceProvider get links %s", link)
             except Exception as err:
                 logging.error("BilibiliVloggerSubscribeSourceProvider get links error:%s", err)
                 return None
@@ -89,4 +158,7 @@ class BilibiliVloggerSubscribeSourceProvider(provider.SourceProvider):
         pass
 
     def load_config(self) -> None:
-        pass
+        cfg = self.config_reader.read()
+        self.sessdata = cfg.get("sessdata", "")
+        if self.sessdata == "":
+            logging.error("SESS_DATA is empty, bilibili_vlogger_subscribe_source_provider will not work")
