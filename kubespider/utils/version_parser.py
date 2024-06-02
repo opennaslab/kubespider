e6fec477820512a56a683c270e342f7e2b86b65d
miRemid <miosuki241@outlook.com>
2024-06-02 15:43:59 +0800

    style: fix pylint
    Signed-off-by: miRemid <miosuki241@outlook.com>

--- kubespider/utils/version_parser.py
+++ kubespider/utils/version_parser.py
@@ -10,7 +10,7 @@ def check_version_at_lest(current_version: str, target_version: str) -> bool:
     while index < current_n and index < target_n:
         if current_vers[index] < target_vers[index]:
             return False
-        elif current_vers[index] > target_vers[index]:
+        if current_vers[index] > target_vers[index]:
             return True
         index += 1
     return current_n >= target_n
@@ -20,9 +20,3 @@ def parse_version(version_name: str) -> Tuple[List[int], int]:
     vers = [int(x) for x in version_name.split('.')]
     return vers, len(vers)
 
-if __name__ == "__main__":
-    assert check_version_at_lest("3.11.2", "3.12.2") == False, "False"
-    assert check_version_at_lest("3.12", "3.12.2") == False, "False"
-    assert check_version_at_lest("3.12.2", "3.12.1") == True, "True"
-    assert check_version_at_lest("3.12.2", "3.12") == True, "True"
-    assert check_version_at_lest("03.12.2", "3.12") == True, "True"
